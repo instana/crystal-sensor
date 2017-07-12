13f650188e5fe321fcc53d9bae0d840482b4a55e
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-07-12 16:52:45 +0200

    Add Opentracing call to example webserver

--- example/webserver.cr
+++ example/webserver.cr
@@ -2,8 +2,13 @@ require "http/server"
 require "../src/instana"
 
 server = HTTP::Server.new(8080) do |context|
+  span = ::Instana.tracer.start_span(:hello_crystal)
+  span.set_tag(:mime, "text/plain")
+  span.set_tag(:code, 200)
+  sleep 0.3
   context.response.content_type = "text/plain"
   context.response.print "Hello Crystal World! The time is #{Time.now}"
+  span.finish
 end
 
 puts "Listening on http://127.0.0.1:8080"
